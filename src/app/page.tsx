49c5a97ce0e1868a7a31998b90ee1c10cc1c4809
Glory <zamaraev@gmail.com>
2024-04-02 23:52:51 +0500

    add test stat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,41 @@
-import { Button, Col, Row, Statistic } from 'antd';
+import { Button, Card, Col, Row, Statistic } from 'antd';
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
   <>
-    <h1>Welcome to NextJS world</h1>;
+    <Head>
+      Welcome to NextJS world
+    </Head>
+    
+
     <div className='m-4'>
       <Row gutter={12}>
-        <Col span={8}>
-          <Statistic title="Active Users" value={112893} />
+        <Col span={4}>
+          <Card bordered={true}>
+            <Statistic title="Отчетов" value={112893} />
+          </Card>
         </Col>
-        <Col span={8}>
-          <Statistic title="Account Balance (CNY)" value={112893} precision={2} />
+        <Col span={4}>
+          <Card bordered={true}>
+            <Statistic title="Авторов" value={112893} precision={2} />
+          </Card>
+        </Col>
+        <Col span={4}>
+          <Card bordered={true}>
+            <Statistic title="Листов карты" value={112893} precision={2} />
+          </Card>
+        </Col>
+        <Col span={4}>
+          <Card bordered={true}>
+            <Statistic title="Субъектов РФ" value={112893} precision={2} />
+          </Card>
+        </Col>
+        <Col span={4}>
+          <Card bordered={true}>
+            <Statistic title="Организаций" value={112893} precision={2} />
+          </Card>
         </Col>
       </Row>    
     </div>
