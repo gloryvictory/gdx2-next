d5b7df59bf39a39014e9c1fdfffb994ec57ff89e
Glory <zamaraev@gmail.com>
2024-04-09 22:27:19 +0500

    add regexp for FIO

--- src/app/author/page.tsx
+++ src/app/author/page.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "antd";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -5,6 +6,11 @@ export interface IAutorRes {
     msg: string
     count: number
     data: string[]
+}
+export interface IData {
+  lastupdate: string
+  id: number
+  author_name: string
 }
 
 async function getData() {
@@ -23,22 +29,28 @@ export const metadata: Metadata = {
     title: "Отчеты | Авторы",
 };
 
+//  проверяем на совпадение регулярного выражения вида "Иванов И.И."
+let sampleRegEx: RegExp = /^[а-яА-ЯёЁa-zA-Z]+ [а-яА-ЯёЁa-zA-Z]. ?[а-яА-ЯёЁa-zA-Z].$/;
+
 export default async function Blog() {
     const authors : IAutorRes = await getData();
     console.log(authors);
-
+    let className1 : string ="box-border rounded bg-slate-100 w-96 pl-2"
   return (
     <>
     <div className="mt-20">
       <h1>Авторы {authors.count}</h1>
 
-      <ul className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-500">
-        {authors.data.map((author: any) => (
-          <li key={author.id}>
-            <Link href={`/author/${author.id}`}>{author.author_name}</Link>
-          </li>
+      {/* <ol type="1" className="marker:text-sky-400 list-disc pl-5 space-y-3 text-slate-500"> */}
+      <ol className="marker:text-sky-400 pl-5 space-y-3 text-slate-500 list-decimal ml-10  ">
+        {authors.data.map((author: any) => (                
+          <Tooltip key={author.id} placement="right" title={ 'Дата обновления: ' + new Date(author.lastupdate).toLocaleDateString('ru-RU')} >
+            <li key={author.id} className={sampleRegEx.test(author.author_name) ? "box-border rounded bg-slate-100 w-96 pl-2" : "box-border rounded bg-slate-100 w-96 pl-2 border-solid border-2 border-fuchsia-600"} > 
+                <Link href={`/author/${author.id}`} className="font-mono">{author.author_name}</Link>
+            </li>
+          </Tooltip>
         ))}
-      </ul>
+      </ol>
     </div>
     </>
   );
