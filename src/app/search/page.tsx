1b0984bb460429f8e58a10c951f92546f8f04aaa
Glory <zamaraev@gmail.com>
2024-05-07 23:43:25 +0500

    add search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,13 @@
 'use client'
 
-
+import { useState, useEffect, Suspense } from 'react'
+import { getReports } from '../actions/getAll'
+import { IReport, IResultReport } from '../types'
+// import Spinner from '@/components/Spinner/Spinner'
+import { Flex, Input, Layout, Space, Spin } from 'antd'
+// import Search from 'antd/es/transfer/search'
+import type { SearchProps } from 'antd/es/input/Search';
+const { Search } = Input;
 // import { useSearchParams } from "next/navigation";
 // import { Suspense } from "react";
 // import LoadingPosts from "./loading";
@@ -8,19 +15,62 @@
 
 // { searchParams,}: {searchParams: { q: string | undefined };}
 
-export default function Search() {
+export default function SearchReport() {
+  const [data, setData] = useState<IResultReport>()
+  const [isLoading, setLoading] = useState<boolean>(false)
   
-  // const searchParams = useSearchParams()!;
-    // const q = searchParams.get("q")!;
+  const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+  
+  const layoutStyle = {
+    // borderRadius: 8,
+    // overflow: 'hidden',
+    marginTop: '16px',
+    marginBottom: '16px',
+    marginLeft: '26px',
+    // position: 'fixed',
+    // position: 'fixed',
+    zIndex: '1',
+    width: 'calc(100% - 30px)',
+    // width: 'calc(100% - 8px)',
+    // maxWidth: 'calc(100% - 8px)',
+  };
 
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(false)
+      const reports = await getReports()
+      setData(reports)
+      setLoading(true)
+    }
+
+    getData()
+  }, [])
+  
   return (
     <>
     <div className="mt-20">
 
-      {/* <Suspense key={q} fallback={<LoadingPosts/>}> */}
-        <h1>Ищем <strong>{"вавапвап"}</strong> </h1>    
-        {/* <TheReport query={q}/> */}
-      {/* </Suspense> */}
+        {/* { !isLoading && <Spinner/>  } */}
+        { !isLoading && <Spin tip="Получаем все отчеты..." size="large" fullscreen/>  }
+
+        <Layout style={layoutStyle}>        
+        {/* <Flex vertical gap={12}> */}
+          <Search placeholder="input search text" onSearch={onSearch}  allowClear   />
+        </Layout>
+        {/* </Flex> */}
+
+        <ol className="marker:text-sky-400 pl-5 space-y-3 text-slate-500 list-decimal ml-10">
+        {/* <ol className="marker:text-sky-400 pl-5 space-y-3 text-slate-500 list-decimal top-24"> */}
+        {
+          data?.data.map((item: IReport ) => (                
+          
+            <li key={item.id} className="box-border rounded bg-slate-100 w-full pl-2 hover:bg-slate-700 hover:text-sky-400 hover:shadow-xl" > 
+                <span className="font-mono" >{item.report_name}</span>
+                {/* onClick={} */}
+            </li>
+          
+        ))}
+      </ol>
       
 
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 const navigation = [
   { name: 'Статистика', href: '/', current: true },
   { name: 'Отчеты', href: '/report_table', current: false },
+  { name: 'Поиск', href: '/search', current: false },
   { name: 'О программе', href: '/about', current: false },
 ]
 
@@ -31,15 +32,15 @@ const Header = () => {
                 d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z"
               />
             </svg>
-             
+
             <span className="text-xl text-white font-bold uppercase tracking-wide">
               Отчеты
             </span>
           </a>
           
-          <div className="text-white text-2xl font-bold">
+          {/* <div className="text-white text-2xl font-bold">
             <TheSearch/>          
-          </div>
+          </div> */}
           
 
 
